e1466a1559a064a6817ab8bc4deb3bd2895ba83a
Harsh1ashra <harsh1.ashra@gmail.com>
2026-01-14 19:20:22 -0500

    Fixing the endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL.trim(),
 });
 
-export const getTasks = () => API.get("/");
-export const createTask = (task) => API.post("/", task);
-export const updateTask = (id, data) => API.put(`/${id}`, data);
-export const deleteTask = (id) => API.delete(`/${id}`);
+export const getTasks = () => API.get("/tasks");
+export const createTask = (task) => API.post("/tasks", task);
+export const updateTask = (id, data) => API.put(`/tasks/${id}`, data);
+export const deleteTask = (id) => API.delete(`/tasks/${id}`);
